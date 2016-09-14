58b71098046dd76873875fd5cba7e8144eeb7491
ken88ling <ken88ling@gmail.com>
2016-09-15 00:07:46 +1000

    + public ActionResult JoinPreLookup() + public ActionResult JoinWithLookup()

--- RealEstate/Rentals/RentalsController.cs
+++ RealEstate/Rentals/RentalsController.cs
@@ -1,94 +1,95 @@
 ﻿using System.Threading.Tasks;
+using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace RealEstate.Rentals
 {
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Web;
-	using System.Web.Mvc;
-	using App_Start;
-	using MongoDB.Bson;
-	using MongoDB.Driver.Builders;
-	using MongoDB.Driver.GridFS;
-	using MongoDB.Driver.Linq;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using App_Start;
+    using MongoDB.Bson;
+    using MongoDB.Driver.Builders;
+    using MongoDB.Driver.GridFS;
+    using MongoDB.Driver.Linq;
 
-	public class RentalsController : Controller
-	{
-		
+    public class RentalsController : Controller
+    {
+
         public readonly RealEstateContextNewApis ContextNew = new RealEstateContextNewApis();
         public readonly RealEstateContext Context = new RealEstateContext();
 
         public async Task<ActionResult> Index(RentalsFilter filters)
-		{
-		    var rentals =await FilterRentals(filters)
-                .Select(r=>new RentalViewModel()
-		        {
-		            Id = r.Id,
+        {
+            var rentals = await FilterRentals(filters)
+                .Select(r => new RentalViewModel()
+                {
+                    Id = r.Id,
                     Address = r.Address,
                     Description = r.Description,
                     NumberOfRooms = r.NumberOfRooms,
                     Price = r.Price
-		        })
-                .OrderBy(r=>r.Price)
-                .ThenByDescending(r=>r.NumberOfRooms)
+                })
+                .OrderBy(r => r.Price)
+                .ThenByDescending(r => r.NumberOfRooms)
                 .ToListAsync();
-            
-			var model = new RentalsList
-			{
-				Rentals = rentals,
-				Filters = filters
-			};
-			return View(model);
-		}
 
-	    private IMongoQueryable<Rental> FilterRentals(RentalsFilter filters)
-	    {
-	        IMongoQueryable<Rental> rentals = ContextNew.Rentals.AsQueryable();
+            var model = new RentalsList
+            {
+                Rentals = rentals,
+                Filters = filters
+            };
+            return View(model);
+        }
 
-			if (filters.MinimumRooms.HasValue)
-			{
-				rentals = rentals
-					.Where(r => r.NumberOfRooms >= filters.MinimumRooms);
-			}
+        private IMongoQueryable<Rental> FilterRentals(RentalsFilter filters)
+        {
+            IMongoQueryable<Rental> rentals = ContextNew.Rentals.AsQueryable();
 
-			if (filters.PriceLimit.HasValue)
-			{
-			    rentals = rentals
-			        .Where(r => r.Price <= filters.PriceLimit);
-			}
-			return rentals;
-		}
+            if (filters.MinimumRooms.HasValue)
+            {
+                rentals = rentals
+                    .Where(r => r.NumberOfRooms >= filters.MinimumRooms);
+            }
 
-		public ActionResult Post()
-		{
-			return View();
-		}
+            if (filters.PriceLimit.HasValue)
+            {
+                rentals = rentals
+                    .Where(r => r.Price <= filters.PriceLimit);
+            }
+            return rentals;
+        }
 
-		[HttpPost]
-		public async Task<ActionResult> Post(PostRental postRental)
-		{
-			var rental = new Rental(postRental);
-			// Context.Rentals.Insert(rental); old api
-		    await ContextNew.Rentals.InsertOneAsync(rental);
-			return RedirectToAction("Index");
-		}
+        public ActionResult Post()
+        {
+            return View();
+        }
 
-		public ActionResult AdjustPrice(string id)
-		{
-			var rental = GetRental(id);
-			return View(rental);
-		}
+        [HttpPost]
+        public async Task<ActionResult> Post(PostRental postRental)
+        {
+            var rental = new Rental(postRental);
+            // Context.Rentals.Insert(rental); old api
+            await ContextNew.Rentals.InsertOneAsync(rental);
+            return RedirectToAction("Index");
+        }
 
-		private Rental GetRental(string id)
-		{
-			//var rental = Context.Rentals.FindOneById(new ObjectId(id));// old api
-		    var rental = ContextNew.Rentals
+        public ActionResult AdjustPrice(string id)
+        {
+            var rental = GetRental(id);
+            return View(rental);
+        }
+
+        private Rental GetRental(string id)
+        {
+            //var rental = Context.Rentals.FindOneById(new ObjectId(id));// old api
+            var rental = ContextNew.Rentals
                 .Find(r => r.Id == id).FirstOrDefault();
 
-			return rental;
-		}
+            return rental;
+        }
 
 
         //[HttpPost] //Replace document
@@ -131,74 +132,114 @@ namespace RealEstate.Rentals
         }
 
         public async Task<ActionResult> Delete(string id)
-		{
-			//Context.Rentals.Remove(Query.EQ("_id", new ObjectId(id))); old api
-		    await ContextNew.Rentals.DeleteOneAsync(r => r.Id == id);
-			return RedirectToAction("Index");
-		}
+        {
+            //Context.Rentals.Remove(Query.EQ("_id", new ObjectId(id))); old api
+            await ContextNew.Rentals.DeleteOneAsync(r => r.Id == id);
+            return RedirectToAction("Index");
+        }
 
-		public string PriceDistribution()
-		{
+        public string PriceDistribution()
+        {
             //use aggregate framework
-			return new QueryPriceDistribution()
+            return new QueryPriceDistribution()
                 //.RunAggregationFluent(ContextNew.Rentals) //new api v2
                 .RunLinq(ContextNew.Rentals) // run Linq
-				//.Run(Context.Rentals) // obsolete api
-				.ToJson();
-		}
+                                             //.Run(Context.Rentals) // obsolete api
+                .ToJson();
+        }
 
-		public ActionResult AttachImage(string id)
-		{
-			var rental = GetRental(id);
-			return View(rental);
-		}
+        public ActionResult AttachImage(string id)
+        {
+            var rental = GetRental(id);
+            return View(rental);
+        }
 
-		[HttpPost]
-		public ActionResult AttachImage(string id, HttpPostedFileBase file)
-		{
-			var rental = GetRental(id);
-			if (rental.HasImage())
-			{
-				DeleteImage(rental);
-			}
-			StoreImage(file, id);
-			return RedirectToAction("Index");
-		}
+        [HttpPost]
+        public ActionResult AttachImage(string id, HttpPostedFileBase file)
+        {
+            var rental = GetRental(id);
+            if (rental.HasImage())
+            {
+                DeleteImage(rental);
+            }
+            StoreImage(file, id);
+            return RedirectToAction("Index");
+        }
 
-		private void DeleteImage(Rental rental)
-		{
-			Context.Database.GridFS.DeleteById(new ObjectId(rental.ImageId));
-			SetRentalImageId(rental.Id, null);
-		}
+        private void DeleteImage(Rental rental)
+        {
+            Context.Database.GridFS.DeleteById(new ObjectId(rental.ImageId));
+            SetRentalImageId(rental.Id, null);
+        }
 
-		private void StoreImage(HttpPostedFileBase file, string rentalId)
-		{
-			var imageId = ObjectId.GenerateNewId();
-			SetRentalImageId(rentalId, imageId.ToString());
-			var options = new MongoGridFSCreateOptions
-			{
-				Id = imageId,
-				ContentType = file.ContentType
-			};
-			Context.Database.GridFS.Upload(file.InputStream, file.FileName, options);
-		}
+        private void StoreImage(HttpPostedFileBase file, string rentalId)
+        {
+            var imageId = ObjectId.GenerateNewId();
+            SetRentalImageId(rentalId, imageId.ToString());
+            var options = new MongoGridFSCreateOptions
+            {
+                Id = imageId,
+                ContentType = file.ContentType
+            };
+            Context.Database.GridFS.Upload(file.InputStream, file.FileName, options);
+        }
 
-		private void SetRentalImageId(string rentalId, string imageId)
-		{
-			var rentalByid = Query<Rental>.Where(r => r.Id == rentalId);
-			var setRentalImageId = Update<Rental>.Set(r => r.ImageId, imageId);
-			Context.Rentals.Update(rentalByid, setRentalImageId);
-		}
+        private void SetRentalImageId(string rentalId, string imageId)
+        {
+            var rentalByid = Query<Rental>.Where(r => r.Id == rentalId);
+            var setRentalImageId = Update<Rental>.Set(r => r.ImageId, imageId);
+            Context.Rentals.Update(rentalByid, setRentalImageId);
+        }
 
-		public ActionResult GetImage(string id)
-		{
-			var image = Context.Database.GridFS
-				.FindOneById(new ObjectId(id));
-			if (image == null)
-			{
-				return HttpNotFound();
-			}
-			return File(image.OpenRead(), image.ContentType);
-		}
-	}
+        public ActionResult GetImage(string id)
+        {
+            var image = Context.Database.GridFS
+                .FindOneById(new ObjectId(id));
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return File(image.OpenRead(), image.ContentType);
+        }
+
+        public ActionResult JoinPreLookup()
+        {
+            var rentals = ContextNew.Rentals.Find(new BsonDocument()).ToList();
+            var rentalZips = rentals.Select(r => r.ZipCode).Distinct().ToArray();
+
+            var zipsById = ContextNew.Database.GetCollection<ZipCode>("zips")
+                .Find(z => rentalZips.Contains(z.Id))
+                .ToList()
+                .ToDictionary(d => d.Id);
+
+            var report = rentals
+                .Select(r => new
+                {
+                    rental = r,
+                    ZipCode = r.ZipCode != null && zipsById.ContainsKey(r.ZipCode)
+                        ? zipsById[r.ZipCode] : null
+                });
+
+            return Content(report.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict }), "application/json");
+        }
+
+        public ActionResult JoinWithLookup()
+        {
+            var report = ContextNew.Rentals
+                .Aggregate()
+                .Lookup<Rental, ZipCode, RentalWithZipCodes>(ContextNew.Database.GetCollection<ZipCode>("zips"),
+                    r => r.ZipCode,
+                    z => z.Id,
+                    w=>w.ZipCodes
+                ).ToList();
+
+            return Content(report.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict }), "application/json");
+        }
+    }
+
+    public class RentalWithZipCodes : Rental
+    {
+        public ZipCode[] ZipCodes { get; set; }
+    }
 }
+
